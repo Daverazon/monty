cec3976936a32750ab44973b3d3ea61d394d56dd
DavidI <kamsidavid2@gmail.com>
2023-09-21 13:00:26 +0100

    perform multiple checks then attempt to run monty bytecode files

--- run_monty.c
+++ run_monty.c
@@ -90,3 +90,67 @@ void (*get_op_func(char *opcode))(stack_t **, unsigned int)
 	}
 	return (NULL);
 }
+
+/**
+ * run_monty - executes monty bytecode script
+ * @script_fd: File descriptor for an open Monty bytecode script
+ * Return: EXIT_SUCCESS on success, else respective error code on failure
+ */
+int run_monty(FILE *script_fd)
+{
+	stack_t *stack = NULL;
+	char *line = NULL;
+	size_t len = 0, exit_status = EXIT_SUCCESS;
+	unsigned int lnumber = 0, prev_token_len = 0;
+	void (*op_func)(stack_t **, unsigned int);
+
+	if (init_stack(&stack) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+
+	while (getline(&line, &len, script_fd) != -1)
+	{
+		lnumber++;
+		op_token = strtow(line, DELIMS);
+		if (op_token == NULL)
+		{
+			if (is_empty_line(line, DELIMS))
+				continue;
+			free_stack(&stack);
+			return (malloc_error());
+		}
+		else if (op_token[0][0] == '#')/*comment line*/
+		{
+			free_tokens();
+			continue;
+		}
+		op_func = get_op_func(op_token[0]);
+		if (op_func == NULL)
+		{
+			free_stack(&stack);
+			exit_status = unknown_op_error(op_token[0], lnumber);
+			free_tokens();
+			break;
+		}
+		prev_token_len = token_arr_len();
+		op_func(&stack, lnumber);
+		if (token_arr_len() != prev_token_len)
+		{
+			if (op_token && op_token[prev_token_len])
+				exit_status = atoi(op_token[prev_token_len]);
+			else
+				exit_status = EXIT_FAILURE;
+			free_tokens();
+			break;
+		}
+		free_tokens();
+	}
+	free_stack(&stack);
+
+	if (line && *line == 0)
+	{
+		free(line);
+		return (malloc_error());
+	}
+	free(line);
+	return (exit_status);
+}
