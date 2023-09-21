427b9e8cfc0c287deefa2d31600721f764df0320
DavidI <kamsidavid2@gmail.com>
2023-09-21 12:37:09 +0100

    match given op code to appropriate function

--- run_monty.c
+++ run_monty.c
@@ -53,3 +53,40 @@ int is_empty_line(char *line, char *delims)
 	}
 	return (1);
 }
+
+/**
+ * get_op_func - Matches an opcode with its corresponding function.
+ * @opcode: The opcode to match
+ * Return: A pointer to the corresponding function
+ */
+void (*get_op_func(char *opcode))(stack_t **, unsigned int)
+{
+	instruction_t op_funcs[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{"sub", sub},
+		{"div", div},
+		{"mul", mul},
+		{"mod", mod},
+		{"pchar", pchar},
+		{"pstr", pstr},
+		{"rotl", rotl},
+		{"rotr", rotr},
+		{"stack", stack},
+		{"queue", queue},
+		{NULL, NULL}
+	};
+	int i;
+
+	for (i = 0; op_funcs[i].opcode; i++)
+	{
+		if (strcmp(opcode, op_funcs[i].opcode) == 0)
+			return (op_funcs[i].f);
+	}
+	return (NULL);
+}
