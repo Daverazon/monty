fef745b38d2f6661fcd9e981cdbb29d811a69b26
DavidI <kamsidavid2@gmail.com>
2023-09-21 13:31:36 +0100

    add pint, swap and pop functions

--- function1.c
+++ function1.c
@@ -72,3 +72,62 @@ void pall(stack_t **top, unsigned int lnumber)
 	}
 	(void)lnumber;
 }
+
+/**
+ * pint - prints the top value of a stack_t linked list
+ * @stack: pointer to the top node of a sack_t linked list
+ * @lnumber: current working line number of a monty bytecode file
+ */
+void pint(stack_t **stack, unsigned int lnumber)
+{
+	if ((*stack)->next = NULL)
+	{
+		set_operror(pint_error(lnumber));
+		return
+	}
+	printf("%d\n", (*stack)->next->n);
+}
+
+/**
+ * pop - removes the top value element of a stack_t linked list
+ * @stack: pointer to top node of a stack_t linked list
+ * @lnumber: current working file number of a monty bytecode fi
+ */
+void pop(stack_t **stack, unsigned int lnumber)
+{
+	stack_t *next = NULL;
+
+	if ((*stack)->next == NULL)
+	{
+		set_operror(pop_error(lnumber));
+		return;
+	}
+	next = (*stack)->next->next;
+	free((*stack)->next);
+	if (next)
+		next->prev = *stack;
+	(*stack)->next = next;
+}
+
+/**
+ * swap - swaps the top two value elements of a stack_t linked list
+ * @stack: a pointer to the top node of a stack_t linked list
+ * @lnumber: current working line number of a monty bytecode fi    le
+ */
+void swap(stack_t **stack, unsigned int lnumber)
+{
+	stack_t *tmp;
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+	{
+		set_operror(short_stack_error(lnumber, "swap"));
+		return;
+	}
+	tmp = (*stack)->next->next;
+	(*stack)->next->next = tmp->next;
+	(*stack)->next->prev = tmp;
+	if (tmp->next)
+		tmp->next->prev = (*stack)->next;
+	tmp->next = (*stack)->next;
+	tmp->prev = *stack;
+	(*stack)->next = tmp;
+}
